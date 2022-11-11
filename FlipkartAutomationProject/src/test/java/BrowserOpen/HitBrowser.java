315f05f50c9f4cccc8504a45ad2bb4f20776db11
Priya Banerjee <109512303+PriyaBanerjee25@users.noreply.github.com>
2022-11-11 22:23:21 +0530

    Changes in HitBrowser

--- FlipkartAutomationProject/src/test/java/BrowserOpen/HitBrowser.java
+++ FlipkartAutomationProject/src/test/java/BrowserOpen/HitBrowser.java
@@ -37,8 +37,9 @@ public class HitBrowser {
 	}
 	//before class to launch all web browser
 	@BeforeTest
-	public void Launchapp() throws Exception {
+	public void Launchapp() throws Exception{
 		WebDriverManager.firefoxdriver().setup();
+		//System.setProperty("webdriver.gecko.driver", "C:\\Users\\priya\\git\\repository\\FlipkartAutomationProject\\driver\\geckodriver.exe");
 		driver = new FirefoxDriver();
 		driver.navigate().to("https://www.flipkart.com");	
 		driver.manage().window().maximize();
@@ -52,52 +53,77 @@ public class HitBrowser {
 	@Test (priority =0)
 	public void Login() throws Exception {
 		WebElement emailaddress= driver.findElement(By.xpath("//input[@class='_2IX_2- VJZDxU']"));
-		emailaddress.sendKeys("9953121985");
+		emailaddress.sendKeys("99531****");
 		WebElement password= driver.findElement(By.xpath("//input[@class='_2IX_2- _3mctLh VJZDxU']"));
-		password.sendKeys("Mousomi@6&&");
+		password.sendKeys("********");
 		WebElement lgnBtn = driver.findElement(By.xpath("//button[@class='_2KpZ6l _2HKlqd _3AWRsL']"));
 		lgnBtn.click();
+		takesnapshot(driver ,"C:\\Users\\priya\\git\\repository\\FlipkartAutomationProject\\ScreenShot\\test.png" );	
 		//Begin A-shot mv_n repository for sreen_sht
-		Screenshot scrnshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(1000)).takeScreenshot(driver);
-		ImageIO.write(scrnshot.getImage(), "jpg", new File("C:\\Users\\priya\\git\\repository\\FlipkartAutomationProject\\ScreenShot\\test1.png"));		
+//		Screenshot scrnshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(1000)).takeScreenshot(driver);
+//		ImageIO.write(scrnshot.getImage(), "jpg", new File("C:\\Users\\priya\\git\\repository\\FlipkartAutomationProject\\ScreenShot\\test1.png"));		
 		//End
 		driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(45, TimeUnit.SECONDS);
 	}
 
-	@Test(priority =2)
-	public void ItemList() throws InterruptedException {
+	@Test(priority =1)
+	public void MenuList() throws InterruptedException {
 		Thread.sleep(2000);
 		java.util.List<WebElement> Products = driver.findElements(By.xpath("/html/body/div/div/div[2]/div"));
 		for(WebElement product:Products) {
 			System.out.println(product.getText());
-			System.out.println("<!--------->>");
+			System.out.println("<!--Main Menu------->>");
 			driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
 			driver.manage().timeouts().pageLoadTimeout(45, TimeUnit.SECONDS);
 		}
 	}
 
-	@Test(priority =1)
-	public void SelectItem() throws InterruptedException {
+	@Test(priority =2)
+	public void SubmenuItem() throws InterruptedException {
 		Thread.sleep(2000);
 		WebDriverWait wait1 = new WebDriverWait(driver, 60);
 		WebElement menu= wait1.until(ExpectedConditions.elementToBeClickable(By.partialLinkText("Electronics")));
-		//WebElement electronic_lpt = wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='_6WOcW9 _2-k99T'][contains(.,'Laptop and Desktop')]")));
-		//WebElement menu = driver.findElement(By.xpath("//span[text()='Electronics' and @class='_1QZ6fC _3Lgyp8']"));
 		Actions action = new Actions(driver);
 		action.moveToElement(menu).click(menu).perform();
-//		WebElement sub_menu = wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Laptop And Desktop' and @class='_6WOcW9]")));
-		WebElement sub_menu =  driver.findElement(By.xpath("//*[@text()='Laptop And Desktop' and @class='_6WOcW9]"));
-		//Actions action = new Actions(driver);
-		//action.moveToElement(menu).perform()//*[@text() = “Submit”]
-		action.moveToElement(sub_menu).click(sub_menu).build().perform();
-		//Actions action = new Actions(driver);
-		//action.moveToElement(electronic).click().build().perform();
-		//action.moveToElement(electronic).moveToElement(electronic_lpt).click().build().perform();
+		java.util.List<WebElement> Products = driver.findElements(By.xpath("/html/body/div/div/div[2]/div"));
+		for(WebElement product:Products) {
+			System.out.println(product.getText());
+			System.out.println("<!----SubMenu Electronics----->>");
+			driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
+			driver.manage().timeouts().pageLoadTimeout(45, TimeUnit.SECONDS);
+		}
+		//WebElement sub_menu = wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//svg[@class='_7DCQC6']")));
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
-		System.out.println("Sucss");
+		System.out.println("Succss");
 	}
+	
+	@Test(priority =3)
+	public void goToMenTopWearSectionFromFashion() throws InterruptedException
+    {
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        WebDriverWait wait1 = new WebDriverWait(driver, 60);
+
+        WebElement FashionHeaderLink= wait1.until(ExpectedConditions.elementToBeClickable(By.partialLinkText("Fashion")));
+        Actions act=new Actions(driver);
+        try {
+        act.moveToElement(FashionHeaderLink).perform();
+        System.out.println("<!----top wera----->>");
+        }
+        catch(Exception e)
+        {
+            act.moveToElement(driver.findElement(By.xpath("//div[@class='_1psGvi SLyWEo']//div[text()='Fashion']"))).perform();
+        }
+        try {
+        	if(driver.findElement(By.linkText("Men's Top Wear")).isDisplayed())
+            {
+                String urlSave = driver.findElement(By.linkText("Men's Top Wear")).getAttribute("href");
+                driver.get(urlSave);
+                System.out.println("<!----Top----->>");
+            }
+        }catch(Exception e) {e.printStackTrace();}  
+    }
 	
 	//after class last 
 	@AfterTest
